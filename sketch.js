f6c5a2f659e51a0138648e52e6b4b07bd1d6286f
Michal8705 <62724357+Michal8705@users.noreply.github.com>
2020-05-18 19:40:21 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,7 +30,7 @@ var r = 0;
 var q = 0;
 
 
-  for(o = 0; o < 2000000; o++) {  
+  for(o = 0; o < 200000; o++) {  
       table1.push(random(50,250));
   
   }
@@ -44,7 +44,7 @@ var q = 0;
 
     }
   
-    for(r = 0; r < 2000000; r++) {  
+    for(r = 0; r < 200000; r++) {  
       table1.push(random(50,250));
     
   }   
