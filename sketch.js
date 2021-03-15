0fe6d3413be7748826cae8667dacadb93879640d
Michal8705 <62724357+Michal8705@users.noreply.github.com>
2021-03-15 20:35:47 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,9 +3,17 @@ function setup() {
 }
 
 function draw() {
-  background(0, 200, 200);
+  background(0, 200, 100);
+   if (window.DeviceOrientationEvent) { 
+  window.addEventListener('orientationchange', function() { 
+   location.reload(); },
+   false); }
 }
 
 function windowResized() {
+ if (window.DeviceOrientationEvent) { 
+  window.addEventListener('orientationchange', function() { 
+   location.reload(); },
+   false); }
   resizeCanvas(windowWidth, windowHeight);
 }
