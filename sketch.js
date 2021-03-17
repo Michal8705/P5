82b5f41b24a4686fd2e27e5d47c3cc6d4924f465
Michal8705 <62724357+Michal8705@users.noreply.github.com>
2021-03-17 21:00:58 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,16 @@
 function setup() {
+  pixelDensity(1);
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(200, 200, 200);
-  text(window.innerHeight,50,50,50,50)
-  text(window.innerWidth,50,70,50,50)
-  text(displayWidth,50,90,50,50)
-  text(displayHeight,50,110,50,50)
+  background(100, 100, 200);
+  text(window.innerWidth,50,50,50,50)
+  text(window.innerHeight,50,70,50,50)
   text(windowWidth,50,130,50,50)
   text(windowHeight,50,150,50,50)
 }
 
 function windowResized() {
-  resizeCanvas(displayWidth,displayHeight);
+  resizeCanvas(windowWidth,windowHeight);
 }
