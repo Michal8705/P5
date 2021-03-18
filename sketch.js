1155f54fc3f6743e9023f727332e590b51e05df6
Michal8705 <62724357+Michal8705@users.noreply.github.com>
2021-03-18 21:51:13 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ function setup() {
 }
 
 function draw() {
-  background(200, 100, 100);
+  background(200, 200, 100);
   text(window.innerWidth,50,50,50,50)
   text(window.innerHeight,50,70,50,50)
   text(windowWidth,50,130,50,50)
