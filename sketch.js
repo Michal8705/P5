0bcd1f20f46ce969d555759a643635f1cccb69e5
Michal8705 <62724357+Michal8705@users.noreply.github.com>
2021-03-21 19:34:46 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,13 +3,15 @@ function setup() {
 }
 
 function draw() {
-  background(200, 200, 200);
-  text(window.innerWidth,50,50,50,50)
-  text(window.innerHeight,50,70,50,50)
+  background(200, 100, 200);
+  text(window.innerWidth,50,90,50,50)
+  text(window.innerHeight,50,110,50,50)
   text(windowWidth,50,130,50,50)
   text(windowHeight,50,150,50,50)
   text(window.screen.width,50,170,50,50)
   text(window.screen.height,50,190,50,50)
+  text(displayWidth,50,210,50,50)
+  text(displayHeight,50,230,50,50)
 }
 
 function windowResized() {
