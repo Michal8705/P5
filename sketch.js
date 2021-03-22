e9c5ce7788ab11341a89bad540aeae1f13fd6f39
Michal8705 <62724357+Michal8705@users.noreply.github.com>
2021-03-22 15:27:36 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function setup() {
 }
 
 function draw() {
-  background(100, 200, 100);
+  background(100, 200, 200);
   text(window.innerWidth,50,90,50,50)
   text(window.innerHeight,50,110,50,50)
   text(windowWidth,50,130,50,50)
@@ -20,8 +20,8 @@ function draw() {
 
   if((document.documentElement.clientWidth != windowWidth ||
      document.documentElement.clientHeight != windowHeight) && a == 1){
-  resizeCanvas(windowWidth,windowHeight);
-  text(document.documentElement.clientHeight, 50,310,50,50);
+  clear();
+  setup();
   a = 0;
   }
 }
