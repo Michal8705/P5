0b72a88e21fdf80de4b137eea4f8d417a5a7b3e2
Michal8705 <62724357+Michal8705@users.noreply.github.com>
2021-03-22 15:42:15 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function setup() {
 }
 
 function draw() {
-  background(100, 200, 200);
+  background(100, 100, 200);
   text(window.innerWidth,50,90,50,50)
   text(window.innerHeight,50,110,50,50)
   text(windowWidth,50,130,50,50)
@@ -17,6 +17,7 @@ function draw() {
   text(displayHeight,50,230,50,50)
   text(document.documentElement.clientWidth, 50,250,50,50);
   text(document.documentElement.clientHeight, 50,270,50,50);
+  text(a, 50,290,50,50);
 
   if((document.documentElement.clientWidth != windowWidth ||
      document.documentElement.clientHeight != windowHeight) && a == 1){
