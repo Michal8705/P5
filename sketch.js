a35e2b359c8634612477ef1859365ef5f270d498
Michal8705 <62724357+Michal8705@users.noreply.github.com>
2022-05-03 18:51:49 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,41 +1,10 @@
-var a = 1;
-var b = 0;
-if(window.devicePixelRatio !== undefined) {
-    dpr = window.devicePixelRatio;
-} else {
-    dpr = 1;
-}
-var screen_width = window.screen.width * dpr;
-var screen_height = window.screen.height * dpr;
-
 function setup() {
-  createCanvas(screen_width, screen_height);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(100, 200, 100);
-  b = b + 1;
-  text(window.innerWidth,50,90,50,50)
-  text(window.innerHeight,50,110,50,50)
-  text(windowWidth,50,130,50,50)
-  text(windowHeight,50,150,50,50)
-  text(window.screen.width,50,170,50,50)
-  text(window.screen.height,50,190,50,50)
-  text(window.visualViewport.width,50,210,50,50)
-  text(window.visualViewport.height,50,230,50,50)
-  text(document.documentElement.clientWidth, 50,250,50,50);
-  text(document.documentElement.clientHeight, 50,270,50,50);
-  text(b, 50,290,50,50);
-
-  // if((document.documentElement.clientWidth != windowWidth ||
-  //    document.documentElement.clientHeight != windowHeight) && a == 1){
-  //  window.restore();  
-  //  a = 0;
-  // }
-}
-
-function windowResized() {
-  // clear();
-  // setup();
-  resizeCanvas(screen_width, screen_height);
+  createCanvas(400, 400);
+  background(220);
+  stroke(255,255,0,255)
+  text(mouseIsPressed,100,100)
 }
